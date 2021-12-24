57ebfc48261d7f3a465ebee79f4634555b99ba70
matheusoc08 <matheuso.c08@gmail.com>
2021-12-23 23:17:33 -0300

    Aula de introdução C# + aula de primeiros passos e estrutura de dados C#

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CSharp;
+using Interface;
+using Enum;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Matheus";
+            person.Idade = 24;
+            person.Estado = "SP";
+
+            var person2 = new Pessoa();
+
+            person2.Nome = "Leonardo";
+            person2.Idade = 23;
+            person2.Estado = "MG";
+
+            Animal animal = new Animal();
+
+            animal.Nome = "Rex";
+            animal.NomeDono = "Pedro";
+            animal.Especie = "Cachorro";
+
+            var pessoa1 = (Pessoas)0;
+
+            Pessoas pessoa2 = Pessoas.João;
+            Pessoas pessoa3 = (Pessoas)3;
+
+            Console.WriteLine(pessoa3);
+                            
+
+        }
+    }
+}
