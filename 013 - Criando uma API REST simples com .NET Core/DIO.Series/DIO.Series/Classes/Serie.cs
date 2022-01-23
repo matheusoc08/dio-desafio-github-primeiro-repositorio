5193d8891ec9c3a0f44990b1449dfce35f912afe
matheusoc08 <matheuso.c08@gmail.com>
2022-01-23 10:49:28 -0300

    Atividade API REST

---
+++ 013 - Criando uma API REST simples com .NET Core/DIO.Series/DIO.Series/Classes/Serie.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido + Environment.NewLine;
+
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public Genero retornaGenero()
+        {
+            return Genero;
+        }
+
+        public string retornaDescricao()
+        {
+            return Descricao;
+        }
+
+        public int retornaAno()
+        {
+            return Ano;
+        }
+
+    }
+}
