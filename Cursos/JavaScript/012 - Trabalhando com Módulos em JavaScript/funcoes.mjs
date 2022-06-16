8ae4f4f320d4b90a0961888cf7771ec7ed5135e5
matheusoc08 <matheuso.c08@gmail.com>
2022-06-15 22:17:22 -0300

    Reorganizacao das pastas dos cursos + clone da pagina inicial do instagram

---
+++ Cursos/JavaScript/012 - Trabalhando com Módulos em JavaScript/funcoes.mjs
@@ -0,0 +1,21 @@
+function mostraIdade(nome, idade){
+
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade){
+
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+export function mostraHobby(nome, hobby){
+
+    return `O hobby de ${nome} é ${hobby}`;
+}
+
+
+export {
+    mostraIdade,
+    mostraCidade
+}
+
---
+++ Cursos/JavaScript/012 - Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobby, mostraIdade } from "./funcoes.mjs";
+
+console.log(mostraIdade("Matheus", 25));
+console.log(mostraCidade("Matheus", "São Paulo"));
+console.log(mostraHobby("Matheus", "Jogar LOL"));
